2213c8c886fe76f5631915d3f55216f5cf56f86c
Caspar Derksen <caspard@me.com>
2019-06-05 19:39:23 +0200

    disable yum repos in box

--- scripts/basebox/disable_yum_repos.sh
+++ scripts/basebox/disable_yum_repos.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -eux
 
-# Configure grub to wait just 1 second before booting
-sed -i 's/^GRUB_TIMEOUT=[0-9]\+$/GRUB_TIMEOUT=1/' /etc/default/grub && grub2-mkconfig -o /boot/grub2/grub.cfg
+# Disable all Yum repos, preventing errors when offline
+yum-config-manager --disable '*'
+
+
 
