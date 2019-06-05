d2617c8c4402f4ee5087875a06c75dcb833e081a
Caspar Derksen <caspard@me.com>
2019-06-05 17:47:01 +0200

    Remove centos7-devbox (preparing for submodule)

--- vagrant/centos7-devbox/ansible/roles/docker/files/docker-clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Stop and remove all containers
-docker ps -a -q | xargs --no-run-if-empty docker stop
-docker ps -a -q | xargs --no-run-if-empty docker rm
-
-# Remove untagged images
-docker images | tail -n +2 | awk '$1 == "<none>" {print $3}' | xargs --no-run-if-empty docker rmi
--- vagrant/centos7-devbox/ansible/roles/openjdk/files/java.sh
+++
@@ -1,2 +0,0 @@
-# /etc/profile.d/java.sh - Managed by Ansible - do not change
-export JAVA_HOME=$(type -p java | xargs readlink -f | xargs dirname | xargs dirname)
