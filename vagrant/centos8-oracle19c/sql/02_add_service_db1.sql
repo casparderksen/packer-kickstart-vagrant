e06b8604df25f0208587d8882696ec2f8f342f4c
Caspar Derksen <caspard@me.com>
2021-10-10 21:59:17 +0200

    fix: add alter session

--- vagrant/centos8-oracle19c/sql/02_add_service_db1.sql
+++ vagrant/centos8-oracle19c/sql/02_add_service_db1.sql
@@ -1,4 +1,10 @@
+DEFINE container = ORCLPDB1
+
+-- Select container
+ALTER SESSION SET CONTAINER=&container;
+
 BEGIN
   DBMS_SERVICE.create_service('db1','db1');
   DBMS_SERVICE.start_service('db1');
 END;
+/
